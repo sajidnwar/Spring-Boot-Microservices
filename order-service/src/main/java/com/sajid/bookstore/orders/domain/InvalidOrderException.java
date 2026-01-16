57c4e319d20044c239e4117c099442569b3622fc
Sajid <sajid321raj@gmail.com>
2026-01-17 02:01:26 +0530

    Added Order module and initial setup

---
+++ order-service/src/main/java/com/sajid/bookstore/orders/domain/InvalidOrderException.java
@@ -0,0 +1,7 @@
+package com.sajid.bookstore.orders.domain;
+
+public class InvalidOrderException extends RuntimeException {
+    public InvalidOrderException(String message) {
+        super(message);
+    }
+}
