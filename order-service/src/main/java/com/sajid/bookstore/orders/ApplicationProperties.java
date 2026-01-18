67a8cb96dc43018f740a1def081150d7a4e8244b
Sajid <sajid321raj@gmail.com>
2026-01-18 16:36:48 +0530

    Added configuration related to order status

---
+++ order-service/src/main/java/com/sajid/bookstore/orders/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.sajid.bookstore.orders;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "orders")
+public record ApplicationProperties(
+        String catalogServiceUrl,
+        String orderEventsExchange,
+        String newOrdersQueue,
+        String deliveredOrdersQueue,
+        String cancelledOrdersQueue,
+        String errorOrdersQueue
+) {
+}
--- order-service/src/main/java/com/sajid/bookstore/orders/OrderServiceApplication.java
+++ order-service/src/main/java/com/sajid/bookstore/orders/OrderServiceApplication.java
@@ -1,9 +1,11 @@
-package com.sajid.bookstore.order_service;
+package com.sajid.bookstore.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
