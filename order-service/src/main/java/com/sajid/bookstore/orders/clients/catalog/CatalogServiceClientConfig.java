572dab706c6f5f17d5dbe6beda6f577e8ea915a7
Sajid <sajid321raj@gmail.com>
2026-01-20 16:51:32 +0530

    Implemented Retry. Circuit breaker and time out

--- order-service/src/main/java/com/sajid/bookstore/orders/clients/catalog/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/sajid/bookstore/orders/clients/catalog/CatalogServiceClientConfig.java
@@ -1,9 +1,13 @@
 package com.sajid.bookstore.orders.clients.catalog;
 
 import com.sajid.bookstore.orders.ApplicationProperties;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class CatalogServiceClientConfig {
@@ -11,7 +15,15 @@ public class CatalogServiceClientConfig {
     @Bean
     RestClient restClient(ApplicationProperties properties) {
 
+        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactoryBuilder.simple()
+                .withCustomizer(customizer -> {
+                    customizer.setConnectTimeout(Duration.ofSeconds(5));
+                    customizer.setReadTimeout(Duration.ofSeconds(5));
+                })
+                .build();
+
         return RestClient.builder().baseUrl(properties.catalogServiceUrl())
+                .requestFactory(requestFactory)
                 .build();
     }
 }
--- order-service/src/main/java/com/sajid/bookstore/orders/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/sajid/bookstore/orders/clients/catalog/ProductServiceClient.java
@@ -1,5 +1,7 @@
 package com.sajid.bookstore.orders.clients.catalog;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,17 +20,21 @@ public class ProductServiceClient {
         this.restClient = restClient;
     }
 
+    @CircuitBreaker(name = "catalog-service")
+    @Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallback")
     public Optional<Product> getProductByCode(String code) {
         log.info("Fetching product for code: {}", code);
-        try {
+
             var product =
                     restClient.get().uri("/api/products/{code}", code).retrieve().body(Product.class);
             return Optional.ofNullable(product);
-        } catch (Exception e) {
-            log.error("Error while fetching product for code: {}", code, e);
-            return Optional.empty();
-        }
 
 
+
+    }
+
+    Optional<Product> getProductByCodeFallback(String code, Throwable t) {
+        log.info("catalog-service get product by code fallback: code:{}, Error: {} ", code, t.getMessage());
+        return Optional.empty();
     }
 }
