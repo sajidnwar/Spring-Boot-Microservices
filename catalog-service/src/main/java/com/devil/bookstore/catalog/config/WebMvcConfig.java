faaf000df35293305f35c7b3d4231cf84ae57c79
Sajid <sajid321raj@gmail.com>
2026-01-29 22:39:44 +0530

    Resolved CORS Error

---
+++ catalog-service/src/main/java/com/devil/bookstore/catalog/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.devil.bookstore.catalog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
+
+
+}
