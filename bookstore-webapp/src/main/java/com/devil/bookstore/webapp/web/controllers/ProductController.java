f48191ad18ae58f6bc5004672a57d22c0056be3d
Sajid <sajid321raj@gmail.com>
2026-01-31 00:56:51 +0530

    Set up a module to work on UI

---
+++ bookstore-webapp/src/main/java/com/devil/bookstore/webapp/web/controllers/ProductController.java
@@ -0,0 +1,18 @@
+package com.devil.bookstore.webapp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping
+    String index() {
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products")
+    String showProductsPage() {
+        return "products";
+    }
+}
---
+++ bookstore-webapp/src/test/java/com/devil/bookstore/webapp/BookstoreWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devil.bookstore.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
