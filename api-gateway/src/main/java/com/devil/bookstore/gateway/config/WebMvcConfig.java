472a62d630b2f574eaa93a421d1854b56c25f1dc
Sajid <sajid321raj@gmail.com>
2026-02-26 12:32:36 +0530

    Fix the cors error as it's defined on service layer as well

---
+++ api-gateway/src/main/java/com/devil/bookstore/gateway/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.devil.bookstore.gateway.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
+                .allowCredentials(false);
+    }
+}
